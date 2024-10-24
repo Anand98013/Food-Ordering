9f81072bd9ae54c1841876511bc5e814b25a726c
Anand Kumar <anandkumarkharahana@gmail.com>
2024-10-24 19:53:10 +0530

    second comment

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:8080';
+export const API_URL =
+  process.env.NODE_ENV === 'dev'
+    ? 'https://food-ordering-backend-production-b900.up.railway.app/'
+    : 'http://localhost:8080';
 
 export const api = axios.create({
   baseURL: API_URL,
